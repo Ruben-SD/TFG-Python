0b602a874112d380c395bc51bb5a053924916abe
Ruben-SD <r.somoza.dominguez@udc.es>
2022-03-07 00:00:54 +0100

    Good frequencies for laptop

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ config = Config.read_config()
 
 frame_timer = FrameTimer()
 
-positioners = [PositionerFactory.create_predictor(config), PositionerFactory.create_tracker(config)]
+positioners = [PositionerFactory.create_predictor(config)]#, PositionerFactory.create_tracker(config)]
 
 while not keyboard.is_pressed('q'):
     delta_time = frame_timer.mark()
     
     for positioner in positioners:
         positioner.update(delta_time)
-    
-    print(f"Predicted position: {positioners[0].get_position()} Tracked position: {positioners[1].get_position()}")
+    positioners[0].get_position()
+    #print(f"Predicted position: {positioners[0].get_position()}")# Tracked position: {positioners[1].get_position()}")
     
 del positioners
 
