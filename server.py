20e448fa92f524d19e0d9ee4873ab2e16e382319
Ruben-SD <r.somoza.dominguez@udc.es>
2022-03-13 19:07:44 +0100

    Tried with rtmixer, big jitter

--- server.py
+++ server.py
@@ -1,17 +1,23 @@
 
+from os import times
 import socket
 import numpy as np
 import pygame as pg
 
 import time
 
-
+start = None
 def play_sound():        
-    audio_samples = get_audio_samples_of_frequencies([18000, 18200, 18400, 18600, 18800, 19000])
+    audio_samples = get_audio_samples_of_frequencies([18000, 18200, 18400, 18600, 18800, 19000, 500, 1000, 1600, 2500])
     sound = pg.mixer.Sound(audio_samples)
     channel = pg.mixer.Channel(0)
+    
+    global start
+    
+    
+    channel.set_volume(1, 0)
+    start = time.time_ns()/1000
     channel.play(sound, -1)
-    channel.set_volume(0.5, 0)
 
 def get_audio_samples_of_frequencies(frequencies):
     # [fStart, fEnd]
@@ -28,7 +34,7 @@ def get_audio_samples_of_frequencies(frequencies):
 
 
 
-pg.mixer.init(frequency=44100, size=-16,channels=2, buffer=4096)
+pg.mixer.init(frequency=44100, size=-16,channels=2, buffer=512)
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -57,29 +63,117 @@ pgChannel = pg.mixer.Channel(0)
 a  = time.time()
 played = False
 
-while True:
-    if time.time() - a > 3 and not played:
-        print("Played")
-        played = True
-        play_sound()
-        start = time.perf_counter_ns()    
-    
-    data = sock.recv(2048)     
-    x = [0, 0, 0, data[0]]
-    s = [0, 0, 0, data[1]]
-    
-    length = int.from_bytes(x, "big")
-    sound2 = int.from_bytes(s, "big")
-    
-    if sound2 != 128:        
-        dt = time.perf_counter_ns() - start
-        print(dt/10e9)
-        print((344.44*100) * (dt/10e9))
-        break
-    
-    if (length != i and i != 0 ):
-        break
-    if length == i:
-        if i == 255:
-            i = -1
-        i += 1
+
+#!/usr/bin/env python3
+"""Play some random bleeps.
+
+This example shows the feature of playing a buffer at a given absolute time.
+Since all play_buffer() invocations are (deliberately) done at once, this puts
+some strain on the "action queue".
+The "qsize" has to be increased in order to handle this.
+
+This example also shows that NumPy arrays can be used, as long as they are
+C-contiguous and use the 'float32' data type.
+
+"""
+
+
+import numpy as np
+import rtmixer
+import sounddevice as sd
+
+seed = 99
+
+device = None
+blocksize = 0
+latency = 'low'
+samplerate = 44100
+
+bleeps = 300
+qsize = 512  # Must be a power of 2
+
+attack = 0.005
+release = 0.1
+pitch_min = 40
+pitch_max = 80
+duration_min = 0.2
+duration_max = 0.6
+amplitude_min = 0.05
+amplitude_max = 0.15
+start_min = 0
+start_max = 10
+channels = None
+
+if duration_min < max(attack, release):
+    raise ValueError('minimum duration is too short')
+
+fade_in = np.linspace(0, 1, num=int(samplerate * attack))
+fade_out = np.linspace(1, 0, num=int(samplerate * release))
+
+r = np.random.RandomState(seed)
+
+bleeplist = []
+
+if channels is None:
+    channels = sd.default.channels['output']
+    if channels is None:
+        channels = sd.query_devices(device, 'output')['max_output_channels']
+
+for _ in range(bleeps):
+    duration = r.uniform(duration_min, duration_max)
+    amplitude = r.uniform(amplitude_min, amplitude_max)
+    pitch = r.uniform(pitch_min, pitch_max)
+    # Convert MIDI pitch (https://en.wikipedia.org/wiki/MIDI_Tuning_Standard)
+    frequency = 2 ** ((pitch - 69) / 12) * 440
+    t = np.arange(int(samplerate * duration)) / samplerate
+    bleep = amplitude * np.sin(2 * np.pi * frequency * t, dtype='float32')
+    bleep[:len(fade_in)] *= fade_in
+    bleep[-len(fade_out):] *= fade_out
+
+    # Note: Arrays must be 32-bit float and C contiguous!
+    assert bleep.dtype == 'float32'
+    assert bleep.flags.c_contiguous
+    bleeplist.append(bleep)
+
+actionlist = []
+
+
+with rtmixer.Mixer(device=device, channels=channels, blocksize=blocksize,
+                samplerate=samplerate, latency=latency, qsize=qsize) as m:
+    while True:
+        if time.time() - a > 3 and not played:
+                start_time = m.time
+                start = time.time_ns()/1000
+                actionlist = [
+                    m.play_buffer(bleep,
+                                    channels=[r.randint(channels) + 1],
+                                    start=start_time + r.uniform(start_min, start_max),
+                                    allow_belated=False)
+                    for bleep in bleeplist
+                ]
+                played = True
+
+        data = sock.recv(2048)     
+        x = [0, 0, 0, data[0]]
+        s = [0, 0, 0, data[9]]
+
+        timestamp = int.from_bytes(data[1:9], "big")
+        times = time.time_ns()/1000
+
+        length = int.from_bytes(x, "big")
+
+        sound2 = int.from_bytes(s, "big")
+
+        if sound2 != 128:        
+            timestamp = int.from_bytes(data[1:9], "big")
+            dt = (timestamp - start)/10e6 + 0.19093565
+            print(dt)
+            print((344.44*100) * dt)
+            break
+
+        if (length != i and i != 0):
+            break
+        if length == i:
+            if i == 255:
+                i = -1
+            i += 1
