cd695e243f1cc5ddda1fbc395fc1ebf3fcd60c18
Ruben-SD <r.somoza.dominguez@udc.es>
2022-05-13 18:19:00 +0200

    Added code, real distance not by side

--- predictor-DESKTOP-IQO8LHC.py
+++ predictor-DESKTOP-IQO8LHC.py
@@ -6,6 +6,63 @@ from receiver import Receiver
 from doppleranalyzer import DopplerAnalyzer
 import numpy as np
 
+class SpeakerDistanceFinder:
+    def __init__(self):
+        self.distances = np.zeros(2)
+        self.time = 0
+        self.is_on_left = True
+        self.delay = False
+        self.all_distances = []
+        self.all_speeds = []
+
+    def update(self, dt, displacements):
+        self.time += dt
+        self.distances += np.abs(displacements)
+
+        self.all_speeds.append(displacements)
+
+        if len(self.all_speeds) > 100:
+            l, r = map(np.array, zip(*self.all_speeds))
+            zero_crossings_l = np.where(np.diff(np.sign(l)))[0]
+            zero_crossings_r = np.where(np.diff(np.sign(r)))[0]
+            inner_left_crosses = []
+            for zcross in zero_crossings_l:
+                if np.all(l[zcross - 20: zcross] < 0): # se estaba acercando? (en el medio)
+                  inner_left_crosses.append(zcross)
+                  print("LEFT")
+                  
+            inner_right_crosses = []
+            for zcross in zero_crossings_r:
+                if np.all(r[zcross - 20: zcross] < 0): # se estaba acercando? (en el medio)
+                  inner_right_crosses.append(zcross)
+                  print("RIGHT")
+
+            total_d = 0
+            for i, inner_left_cross in enumerate(inner_left_crosses[:-1]):
+                d = np.sum(l[inner_left_cross:inner_right_crosses[i]])
+                total_d += d
+            computed_distance = total_d/len(inner_left_crosses)
+            print("TOTAL AVG distance = ", computed_distance)
+
+        else:
+            print(len(self.all_speeds))
+        # if self.time > 0.5 and self.is_on_left and displacements[0] > 0 and self.distances[0] > 5:
+        #     self.time = 0            
+        #     distance = self.distances[0]
+        #     self.all_distances.append(distance)
+        #     print(distance)
+        #     self.distances = displacements
+        #     self.is_on_left = False
+        # if self.time > 0.5 and not self.is_on_left and displacements[1] > 0 and self.distances[1] > 5:
+        #     self.time = 0
+        #     distance = self.distances[1]
+        #     self.all_distances.append(distance)
+        #     print(distance)
+        #     self.distances = displacements
+        #     self.is_on_left = True 
+        # print("Distance: ", np.mean(self.all_distances))
+
+
 class Predictor(Positioner):
     def __init__(self, config, plotter):
         super().__init__(config, plotter)
@@ -18,6 +75,7 @@ class Predictor(Positioner):
         
         self.receiver = Receiver()
         self.doppler_analyzers = [DopplerAnalyzer(speaker.get_config().get_frequencies(), plotter, config) for speaker in self.speakers]
+        self.speaker_distance_finder = SpeakerDistanceFinder()
         # for i, speaker in enumerate(self.speakers):
         #     plotter.add_data(f'predicted_x_position_{i}', [], plot=True)
         
@@ -47,10 +105,11 @@ class Predictor(Positioner):
             # deg1 = np.arctan2(xR, yR)*180.0/np.pi
             # print("this", deg, deg1)
             #print("POS: ", x, y, xR, yR, "ANGLES: ", np.arccos(cosines[0]) *180/np.pi, np.arccos(cosines[1]) * 180/np.pi, "COS: ", cosines)
-            speeds = np.array([doppler_analyzer.extract_speeds_from(sound_samples, 1) for i, doppler_analyzer in enumerate(self.doppler_analyzers)])
+            speeds = np.array([doppler_analyzer.extract_speeds_from(sound_samples, cosines[i]) for i, doppler_analyzer in enumerate(self.doppler_analyzers)])
         else:
             speeds = np.array([doppler_analyzer.extract_speeds_from(sound_samples, None) for i, doppler_analyzer in enumerate(self.doppler_analyzers)])
         self.position.move_by((-np.array(speeds) * dt))
+        self.speakers_distance = self.speaker_distance_finder.update(dt, speeds)
         # for i, _ in enumerate(self.speakers):
         #     plotter.add_sample(f'predicted_x_position_{i}', self.get_distance()[i])
         # if self.two_dimensions:
